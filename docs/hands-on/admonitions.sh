e2bc2f9d93223d409dfb9d90f344911c885b2821
yangsl <3242809681@qq.com>
2024-09-24 01:03:34 +0800

    add admonitions format convert shell script

---
+++ docs/hands-on/admonitions.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# 将水源文档的 admonitions 格式转换为 mkdocs-material 主题
+
+OS_TYPE=$(uname)
+
+FILE_list=(
+    "md-handson-notes.md"
+    "dft-handson-notes.md"
+)
+
+for file in "${FILE_list[@]}"; do
+    if [[ "$OS_TYPE" == "Darwin" ]]; then
+        if grep -q ":::" "$file"; then
+            sed -i '' 's/:::danger/!!! danger ""/g' "$file"
+            sed -i '' 's/:::info/!!! info ""/g' "$file"
+            sed -i '' 's/:::warning/!!! warning ""/g' "$file"
+            sed -i '' 's/:::success/!!! success ""/g' "$file"
+
+            sed -i '' 's/::://g' "$file"
+
+            sed -i '' '/!!!/ {
+                N
+                s/\n/\n\n    /
+            }' "$file"
+
+            echo "adolitions format has been converted to mkdocs-material format."
+        else 
+            echo "No need."
+        fi
+
+    elif [[ "$OS_TYPE" == "Linux" ]]; then
+        if grep -q ":::" "$file"; then
+            sed -i 's/:::danger/!!! danger ""/g' "$file"
+            sed -i 's/:::info/!!! info ""/g' "$file"
+            sed -i 's/:::warning/!!! warning ""/g' "$file"
+            sed -i 's/:::success/!!! success ""/g' "$file"
+
+            sed -i 's/::://g' "$file"
+
+            sed -i '' '/!!!/ {
+                N
+                s/\n/\n\n    /
+            }' "$file"
+
+            echo "adolitions format has been converted to mkdocs-material format."
+        else 
+            echo "No need"
+        fi
+    else
+        echo "Unsupported OS: $OS_TYPE"
+        exit 1
+    fi
+done
