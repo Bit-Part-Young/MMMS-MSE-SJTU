261ae54464faa1eed14b1a34532c820a5a4a9b58
yangsl <3242809681@qq.com>
2025-01-08 14:57:02 +0800

    update admonitions.sh

--- docs/hands-on/admonitions.sh
+++ docs/hands-on/admonitions.sh
@@ -4,51 +4,36 @@
 
 OS_TYPE=$(uname)
 
-FILE_list=(
+if [[ "$OS_TYPE" == "Darwin" ]]; then
+    SED="sed -i ''"
+elif [[ "$OS_TYPE" == "Linux" ]]; then
+    SED="sed -i"
+else
+    echo "Unsupported OS: $OS_TYPE"
+    exit 1
+fi
+
+file_list=(
     "md-handson-notes.md"
     "dft-handson-notes.md"
 )
 
-for file in "${FILE_list[@]}"; do
-    if [[ "$OS_TYPE" == "Darwin" ]]; then
-        if grep -q ':::' "$file"; then
-            sed -i '' 's/:::danger/!!! danger ""/g' "$file"
-            sed -i '' 's/:::info/!!! info ""/g' "$file"
-            sed -i '' 's/:::warning/!!! warning ""/g' "$file"
-            sed -i '' 's/:::success/!!! success ""/g' "$file"
+for file in "${file_list[@]}"; do
+  if grep -q ":::" "$file"; then
+      SED 's/:::danger/!!! danger ""/g' "$file"
+      SED 's/:::info/!!! info ""/g' "$file"
+      SED 's/:::warning/!!! warning ""/g' "$file"
+      SED 's/:::success/!!! success ""/g' "$file"
 
-            sed -i '' 's/::://g' "$file"
+      SED 's/::://g' "$file"
 
-            sed -i '' '/!!!/ {
-                N
-                s/\n/\n\n    /
-            }' "$file"
+      SED '' '/!!!/ {
+          N
+          s/\n/\n\n    /
+      }' "$file"
 
-            echo "$file admonitions format converted to mkdocs-material."
-        else 
-            echo "No need to convert."
-        fi
-
-    elif [[ "$OS_TYPE" == "Linux" ]]; then
-        if grep -q ":::" "$file"; then
-            sed -i 's/:::danger/!!! danger ""/g' "$file"
-            sed -i 's/:::info/!!! info ""/g' "$file"
-            sed -i 's/:::warning/!!! warning ""/g' "$file"
-            sed -i 's/:::success/!!! success ""/g' "$file"
-
-            sed -i 's/::://g' "$file"
-
-            sed -i '' '/!!!/ {
-                N
-                s/\n/\n\n    /
-            }' "$file"
-
-            echo "adolitions format has been converted to mkdocs-material format."
-        else 
-            echo "No need"
-        fi
-    else
-        echo "Unsupported OS: $OS_TYPE"
-        exit 1
-    fi
+      echo "adolitions format has been converted to mkdocs-material format."
+  else
+      echo "No need"
+  fi
 done
